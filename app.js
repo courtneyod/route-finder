fa8088bef659b73321f2142233b1c6644913c863
COURTNEY <courtney.od@gmail.com>
2016-10-14 16:40:41 -0700

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict"
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/public', express.static('public'))
 
