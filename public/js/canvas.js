0ac0425205a48ac497bf76d590199d0faf88db4b
COURTNEY <courtney.od@gmail.com>
2016-10-25 16:36:05 -0700

    graph is working

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,25 +1,14 @@
 "use strict"
 
-
-// canvasContext();
-// function canvasContext() {
-//     var canvas = document.getElementById("graph");
-//     //console.log(graph, "hi")
-//     var context = canvas.getContext('2d');
-//     console.log(context)
-//     context.fillStyle = "#FF0000";
-//     context.fillRect(0,0,200,300);
-// };
-
 var graph;
 var xPadding = 30;
 var yPadding = 30;
 
 
 function parseDataCanvas(dataRoute){
-  console.log(dataRoute)
+  //console.log(dataRoute, "this is dataRoute")
   var dataR = {values: []};
-  for (var i = 3; i < dataRoute.length - 300; i+= 100) {
+  for (var i = 3; i < dataRoute.length - 2; i+= 1) {
     if (dataRoute[i].d !== undefined && dataRoute[i].e !== undefined){
     dataR.values.push({
       x: (dataRoute[i].d*(0.000621)).toFixed(1),
@@ -27,7 +16,7 @@ function parseDataCanvas(dataRoute){
     });
   }
   }
-  console.log(dataR, "this is data values")
+  //console.log(dataR, "this is data values")
   drawGraph(dataR)
   return dataR;
 };
@@ -38,32 +27,39 @@ function parseDataCanvas(dataRoute){
 // Returns the max Y value in our data list
   function getMaxY(data) {
       var max = 0;
+      console.log(data, "for max loop", data.values.length)
 
-      for(var i = 0; i < data.values.length; i ++) {
-          if(data.values[i].y > max) {
-              max = data.values[i].y;
-          }
-      }
+      var possibleMaxNumbers = data.values.map(function(cur){
+        return cur.y
+      })
+      max = Math.max.apply(null, possibleMaxNumbers)
 
-      max += 10 - max % 10;
+      // for(var i = 0; i < data.values.length; i ++) {
+      //     if(data.values[i].y > max) {
+      //         max = data.values[i].y;
+      //     }
+      // }
+
+      console.log(max, "this is the max")
       return max;
-    }
+  }
 
-    // Return the x pixel for a graph point
-    function getXPixel(val, data) {
-        return ((graph.width() - xPadding) / data.values.length) * val + (xPadding * 1.5);
-    }
+  // Return the x pixel for a graph point
+  function getXPixel(val, data) {
+      return ((graph.width() - xPadding) / data.values.length) * val + (xPadding * 1.5);
+  }
 
-    // Return the y pixel for a graph point
-    function getYPixel(val, data) {
-        return graph.height() - (((graph.height() - yPadding) / getMaxY(data)) * val) - yPadding;
-    }
+  // Return the y pixel for a graph point
+  function getYPixel(val, data, maxY) {
+      return graph.height() - (((graph.height() - yPadding) / maxY) * val) - yPadding;
+  }
 
 function drawGraph(data){
     var graphContainerDiv = $('.graph-container');
-    graphContainerDiv.append('<canvas id="graph" width="900" height="100" style="border:1px solid #ccc; padding: 10px;"></canvas>');
-    console.log(data.values, "hihihihih")
+    graphContainerDiv.append('<canvas id="graph" width="900" height="400" style="border:1px solid #ccc; padding: 10px;"></canvas>');
+    //console.log(data.values, "hihihihih")
     graph = $('#graph');
+    //console.log(graph)
     var context = graph[0].getContext('2d');
 
     context.lineWidth = 2;
@@ -79,25 +75,25 @@ function drawGraph(data){
     context.stroke();
 
     // Draw the X value texts
-    for(var i = 0; i < data.values.length; i ++) {
+    for(var i = 0; i < data.values.length; i += 500) {
         context.fillText(data.values[i].x, getXPixel(i, data), graph.height() - yPadding + 20);
     }
 
     // Draw the Y value texts
     context.textAlign = "right"
     context.textBaseline = "middle";
-
-    for(var i = 0; i < getMaxY(data); i += 10) {
-        context.fillText(i, xPadding - 10, getYPixel(i, data));
+    var maxY = getMaxY(data)
+    for(var i = 0; i < maxY; i += 100) {
+        context.fillText(i, xPadding - 10, getYPixel(i, data, maxY) );
     }
 
     context.strokeStyle = '#f00';
 
     // Draw the line graph
     context.beginPath();
-    context.moveTo(getXPixel(0, data), getYPixel(data.values[0].y, data));
+    context.moveTo(getXPixel(0, data), getYPixel(data.values[0].y, data, maxY));
     for(var i = 1; i < data.values.length; i ++) {
-        context.lineTo(getXPixel(i, data), getYPixel(data.values[i].y, data));
+        context.lineTo(getXPixel(i, data), getYPixel(data.values[i].y, data, maxY));
     }
     context.stroke();
 
@@ -106,7 +102,7 @@ function drawGraph(data){
 
     for(var i = 0; i < data.values.length; i ++) {
         context.beginPath();
-        context.arc(getXPixel(i, data), getYPixel(data.values[i].y, data), 4, 0, Math.PI * 2, true);
+        context.arc(getXPixel(i, data), getYPixel(data.values[i].y, data, maxY), 4, 0, Math.PI * 2, true);
         context.fill();
     }
 }
